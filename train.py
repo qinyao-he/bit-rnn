8403058f9d60a2b9f8f8c8488249eb53f37f78c6
何钦尧 <hqythu@gmail.com>
2016-11-16 13:27:00 +0800

    add 2w2f

---
+++ train.py
@@ -0,0 +1,200 @@
+import time
+import functools
+
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.models.rnn.ptb import reader
+
+import bit_utils
+from bit_rnn_cell import BitGRUCell
+
+flags = tf.flags
+logging = tf.logging
+
+flags.DEFINE_string(
+        "model", "small",
+        "A type of model. Possible options are: small, medium, large.")
+flags.DEFINE_string("data_path", None, "data_path")
+
+FLAGS = flags.FLAGS
+
+
+class PTBModel(object):
+    """The PTB model."""
+
+    def __init__(self, is_training, config):
+        self.batch_size = batch_size = config.batch_size
+        self.num_steps = num_steps = config.num_steps
+        size = config.hidden_size
+        vocab_size = config.vocab_size
+
+        self._input_data = tf.placeholder(tf.int32, [batch_size, num_steps])
+        self._targets = tf.placeholder(tf.int32, [batch_size, num_steps])
+
+        # Slightly better results can be obtained with forget gate biases
+        # initialized to 1 but the hyperparameters of the model would need to be
+        # different than reported in the paper.
+        lstm_cell = BitGRUCell(size, w_bit=2, f_bit=2)
+        if is_training and config.keep_prob < 1:
+            lstm_cell = tf.nn.rnn_cell.DropoutWrapper(
+                    lstm_cell, output_keep_prob=config.keep_prob)
+        cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * config.num_layers)
+
+        self._initial_state = cell.zero_state(batch_size, tf.float32)
+        self._initial_state = bit_utils.round_bit(tf.sigmoid(self._initial_state), bit=2)
+
+        with tf.device("/cpu:0"):
+            embedding = tf.get_variable("embedding",
+                                        [vocab_size, size],
+                                        initializer=tf.random_uniform_initializer())
+            inputs = tf.nn.embedding_lookup(embedding, self._input_data)
+
+        inputs = bit_utils.round_bit(tf.nn.relu(inputs), bit=2)
+
+        if is_training and config.keep_prob < 1:
+            inputs = tf.nn.dropout(inputs, config.keep_prob)
+
+        inputs = [tf.squeeze(input_, [1])
+                  for input_ in tf.split(1, num_steps, inputs)]
+        outputs, state = tf.nn.rnn(cell, inputs, initial_state=self._initial_state)
+
+        output = tf.reshape(tf.concat(1, outputs), [-1, size])
+        with bit_utils.replace_variable(lambda x: bit_utils.quantize_w(tf.tanh(x), bit=2)):
+            softmax_w = tf.get_variable("softmax_w", [size, vocab_size])
+        softmax_b = tf.get_variable("softmax_b", [vocab_size])
+        logits = tf.matmul(output, softmax_w) + softmax_b
+        loss = tf.nn.seq2seq.sequence_loss_by_example(
+                [logits],
+                [tf.reshape(self._targets, [-1])],
+                [tf.ones([batch_size * num_steps])])
+        self._cost = cost = tf.reduce_sum(loss) / batch_size
+        self._final_state = state
+
+        if not is_training:
+            return
+
+        self._lr = tf.Variable(0.0, trainable=False)
+        tvars = tf.trainable_variables()
+        grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars), config.max_grad_norm)
+        optimizer = tf.train.AdamOptimizer(self.lr)
+        self._train_op = optimizer.apply_gradients(zip(grads, tvars))
+
+    def assign_lr(self, session, lr_value):
+        session.run(tf.assign(self.lr, lr_value))
+
+    @property
+    def input_data(self):
+        return self._input_data
+
+    @property
+    def targets(self):
+        return self._targets
+
+    @property
+    def initial_state(self):
+        return self._initial_state
+
+    @property
+    def cost(self):
+        return self._cost
+
+    @property
+    def final_state(self):
+        return self._final_state
+
+    @property
+    def lr(self):
+        return self._lr
+
+    @property
+    def train_op(self):
+        return self._train_op
+
+
+class BitConfig(object):
+    learning_rate =1e-3
+    max_grad_norm = 10
+    num_layers = 1
+    num_steps = 20
+    hidden_size = 300
+    max_epoch = 100
+    keep_prob = 0.5
+    batch_size = 20
+    vocab_size = 10000
+
+
+def run_epoch(session, m, data, eval_op, verbose=False):
+    """Runs the model on the given data."""
+    epoch_size = ((len(data) // m.batch_size) - 1) // m.num_steps
+    start_time = time.time()
+    costs = 0.0
+    iters = 0
+    state = m.initial_state.eval()
+    for step, (x, y) in enumerate(reader.ptb_iterator(data, m.batch_size, m.num_steps)):
+        cost, state, _ = session.run([m.cost, m.final_state, eval_op],
+                                     {m.input_data: x,
+                                      m.targets: y,
+                                      m.initial_state: state})
+        costs += cost
+        iters += m.num_steps
+
+        if verbose and step % (epoch_size // 10) == 10:
+            print("%.3f perplexity: %.3f speed: %.0f wps" %
+                  (step * 1.0 / epoch_size, np.exp(costs / iters),
+                   iters * m.batch_size / (time.time() - start_time)))
+
+    return np.exp(costs / iters)
+
+
+def get_config():
+    return BitConfig()
+
+
+def main(_):
+    if not FLAGS.data_path:
+        raise ValueError("Must set --data_path to PTB data directory")
+
+    raw_data = reader.ptb_raw_data(FLAGS.data_path)
+    train_data, valid_data, test_data, _ = raw_data
+
+    config = get_config()
+    eval_config = get_config()
+    eval_config.batch_size = 1
+    eval_config.num_steps = 1
+
+    with tf.Graph().as_default(), tf.Session() as session:
+        initializer = tf.uniform_unit_scaling_initializer()
+        with tf.variable_scope("model", reuse=None, initializer=initializer):
+            m = PTBModel(is_training=True, config=config)
+        with tf.variable_scope("model", reuse=True, initializer=initializer):
+            mvalid = PTBModel(is_training=False, config=config)
+            mtest = PTBModel(is_training=False, config=eval_config)
+
+        tf.initialize_all_variables().run()
+
+        def get_learning_rate(epoch):
+            base_lr = 1e-3
+            if epoch <= 40:
+                return base_lr
+            elif epoch <= 80:
+                return base_lr * 0.1
+            else:
+                return base_lr * 0.01
+
+        for i in range(config.max_epoch):
+            m.assign_lr(session, get_learning_rate(i))
+
+            print("Epoch: %d Learning rate: %.3f" % (i + 1, session.run(m.lr)))
+            train_perplexity = run_epoch(session, m, train_data, m.train_op,
+                                         verbose=True)
+            print("Epoch: %d Train Perplexity: %.3f" % (i + 1, train_perplexity))
+            valid_perplexity = run_epoch(session, mvalid, valid_data, tf.no_op())
+            print("Epoch: %d Valid Perplexity: %.3f" % (i + 1, valid_perplexity))
+
+        test_perplexity = run_epoch(session, mtest, test_data, tf.no_op())
+        print("Test Perplexity: %.3f" % test_perplexity)
+
+
+if __name__ == "__main__":
+    tf.app.run()
